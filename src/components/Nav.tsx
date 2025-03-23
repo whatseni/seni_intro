20b21cde001f74cfb78081e619882d183024dc4d
seeys <es2008e@naver.com>
2025-03-23 13:13:10 +0900

    0323 :: Nav css 적용 완료료

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,16 +5,16 @@ import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
 export default function Nav() {
   return (
     <nav className="fixed bottom-8 left-1/2 -translate-x-1/2 z-10 flex gap-3 px-7 py-3 rounded-full bg-black/30 backdrop-blur-md w-max">
-      <Link href="/" className="p-3.5 rounded-full text-lg text-[var(--color-light)] flex bg-transparent hover:bg-black/30">
+      <Link href="/" className="p-4 rounded-full flex items-center justify-center text-[var(--color-light)] text-lg bg-transparent hover:bg-black/30 transition active:bg-[var(--color-primary)] active:text-[var(--color-bg)]">
         <AiOutlineHome />
       </Link>
-      <Link href="/about" className="p-3.5 rounded-full text-lg text-[var(--color-light)] flex bg-transparent hover:bg-black/30">
+      <Link href="/" className="p-4 rounded-full flex items-center justify-center text-[var(--color-light)] text-lg bg-transparent hover:bg-black/30 transition active:bg-[var(--color-primary)] active:text-[var(--color-bg)]">
         <AiOutlineUser />
       </Link>
-      <Link href="/project" className="p-3.5 rounded-full text-lg text-[var(--color-light)] flex bg-transparent hover:bg-black/30">
+      <Link href="/" className="p-4 rounded-full flex items-center justify-center text-[var(--color-light)] text-lg bg-transparent hover:bg-black/30 transition active:bg-[var(--color-primary)] active:text-[var(--color-bg)]">
         <BiBook />
       </Link>
-      <Link href="/contact" className="p-3.5 rounded-full text-lg text-[var(--color-light)] flex bg-transparent hover:bg-black/30">
+      <Link href="/" className="p-4 rounded-full flex items-center justify-center text-[var(--color-light)] text-lg bg-transparent hover:bg-black/30 transition active:bg-[var(--color-primary)] active:text-[var(--color-bg)]">
         <BiMessageSquareDetail />
       </Link>
     </nav>
